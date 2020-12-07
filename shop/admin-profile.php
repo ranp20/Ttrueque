b7c929adcb7c9a69e1e125c01ac8fe495f477797
Ttito19 <andrezchavez.2000@gmail.com>
2020-12-07 01:00:05 -0500

    update profile client

--- shop/admin-profile.php
+++ shop/admin-profile.php
@@ -32,19 +32,30 @@ $d = $c->get_data_by_id($_SESSION['user']);
                     <div class="content-title-profile">
                         <h1 class="title-dashboard lang_ttrq" key="title-form-1-cli-dp-ad_cli">Mi perfil</h1>
                     </div>
-                    <form action="php/update_user-profile.php" method="POST" ectype="multipart/form-data">
+                    <form id="form-perfilcliente" method="POST" ectype="multipart/form-data">
                         <input type="hidden" name="id_cli" value="<?php echo $d[0]['id_cliente']; ?>"></input>
                         <!-- INFORMACIÓN BÁSICA -->
                         <div class="profile-content">
-                            <div class="profile-head lang_ttrq" key="subtitle-form-1-cli-dp-ad_cli">Información Básica</div>
+                            <div class="profile-head lang_ttrq" key="subtitle-form-1-cli-dp-ad_cli">Información Básica
+                            </div>
                             <div class="form-content-profile">
                                 <div class="profile-controls">
-                                    <label for="" class="label-profile lang_ttrq" key="txt-form-1-cli-dp-ad_cli-1">Tu nombre</label>
-                                    <input type="text" readonly name="name_cli" class="input-profile" value="<?php echo $d[0]['nombre_cliente'] . " " . $d[0]['apellido_cliente']; ?>">
+                                    <label for="" class="label-profile lang_ttrq" key="txt-form-1-cli-dp-ad_cli-1">Tu
+                                        nombre</label>
+                                    <input type="text" name="name_cli" class="input-profile"
+                                        value="<?php echo $d[0]['nombre_cliente']  ?>">
                                 </div>
                                 <div class="profile-controls">
-                                    <label for="" class="label-profile lang_ttrq" key="txt-form-1-cli-dp-ad_cli-2">Tu correo electrónico</label>
-                                    <input type="text" readonly class="input-profile" value="<?php echo $d[0]['email_cliente']; ?>">
+                                    <label for="" class="label-profile lang_ttrq" key="txt-form-1-cli-dp-ad_cli-1">Tu
+                                        nombre</label>
+                                    <input type="text" name="lastaname_cli" class="input-profile"
+                                        value="<?php echo  $d[0]['apellido_cliente']; ?>">
+                                </div>
+                                <div class="profile-controls">
+                                    <label for="" class="label-profile lang_ttrq" key="txt-form-1-cli-dp-ad_cli-2">Tu
+                                        correo electrónico</label>
+                                    <input type="text" class="input-profile" name="email"
+                                        value="<?php echo $d[0]['email_cliente']; ?>">
                                 </div>
                                 <!-- <div class="profile-controls">
                                   <label for="" class="label-profile">Tu contraseña</label>
@@ -62,22 +73,33 @@ $d = $c->get_data_by_id($_SESSION['user']);
                             <div class="form-content-profile">
 
                                 <div class="profile-controls">
-                                    <label for="" class="label-profile lang_ttrq" key="txt-form-2-cli-de-ad_cli-1">País</label>
-                                    <input type="text" readonly name="country_cli" class="input-profile" value="<?php echo $d[0]['nombre_pais']; ?>">
+                                    <label for="" class="label-profile lang_ttrq"
+                                        key="txt-form-2-cli-de-ad_cli-1">País</label>
+                                    <select name="country_cli" class="input-profile">
+                                        <option value="1">Perú</option>
+                                        <option value="2">PerúArgentina</option>
+                                    </select>
+
+
                                 </div>
                                 <div class="profile-controls">
-                                    <label for="" class="label-profile lang_ttrq" key="txt-form-2-cli-de-ad_cli-2">Dirección</label>
-                                    <input type="text" readonly name="city_cli" class="input-profile" value="<?php echo $d[0]['direccion_cliente']; ?>">
+                                    <label for="" class="label-profile lang_ttrq"
+                                        key="txt-form-2-cli-de-ad_cli-2">Dirección</label>
+                                    <input type="text" name="city_cli" class="input-profile"
+                                        value="<?php echo $d[0]['direccion_cliente']; ?>">
                                 </div>
 
                                 <div class="profile-controls">
-                                    <label for="" class="label-profile lang_ttrq" key="txt-form-2-cli-de-ad_cli-3">Teléfono</label>
-                                    <input type="text" readonly name="telephone_cli" class="input-profile" value="<?php echo $d[0]['telefono']; ?>">
+                                    <label for="" class="label-profile lang_ttrq"
+                                        key="txt-form-2-cli-de-ad_cli-3">Teléfono</label>
+                                    <input type="text" name="telephone_cli" class="input-profile"
+                                        value="<?php echo $d[0]['telefono']; ?>">
                                 </div>
                             </div>
                         </div>
                         <div class="profile-controls-submit">
-                            <button type="submit" class="btn-submit-profile lang_ttrq" key="btn-updt-cli-form-1-ad_cli">Actualizar</button>
+                            <button type="submit" class="btn-submit-profile lang_ttrq" key="btn-updt-cli-form-1-ad_cli"
+                                id="btn-update-perfilcliente">Actualizar</button>
                         </div>
                     </form>
                 </div>
@@ -91,14 +113,19 @@ $d = $c->get_data_by_id($_SESSION['user']);
                         <!-- INFORMACIÓN BÁSICA -->
                         <input type="hidden" id="tienda" name="tienda" value="<?php echo $d[0]['tienda']; ?>">
                         <div class="store-config-content">
-                            <div class="store-config-head lang_ttrq" key="subtitle-form-3-cli-ad_cli">Información Básica</div>
+                            <div class="store-config-head lang_ttrq" key="subtitle-form-3-cli-ad_cli">Información Básica
+                            </div>
                             <div class="form-content-store-config">
                                 <div class="store-config-controls-img">
-                                    <label for="" class="label-store-config-img lang_ttrq" key="txt-form-3-cli-ad_cli-1">Logo</label>
-                                    <input type="file" name="logo" id="logo-store" class="input-store-config-img logo-store">
+                                    <label for="" class="label-store-config-img lang_ttrq"
+                                        key="txt-form-3-cli-ad_cli-1">Logo</label>
+                                    <input type="file" name="logo" id="logo-store"
+                                        class="input-store-config-img logo-store">
                                     <div class="container-upd-logo-store_ttrq">
                                         <div class="content-upd-logo-store_ttrq">
-                                            <div style="background-image: url(./images/store/<?php echo $d[0]['logo']; ?>);background-repeat:no-repeat;background-size: contain;background-position: center;"></div>
+                                            <div
+                                                style="background-image: url(./images/store/<?php echo $d[0]['logo']; ?>);background-repeat:no-repeat;background-size: contain;background-position: center;">
+                                            </div>
                                         </div>
                                     </div>
                                     <input type="hidden" name="logo" id="logostr">
@@ -106,7 +133,8 @@ $d = $c->get_data_by_id($_SESSION['user']);
                             </div>
                         </div>
                         <div class="store-config-controls-submit">
-                            <button type="submit" id="btn_update-store" class="btn-submit-store-config lang_ttrq" key="btn-updt-cli-form-2-ad_cli">Actualizar</button>
+                            <button type="submit" id="btn_update-store" class="btn-submit-store-config lang_ttrq"
+                                key="btn-updt-cli-form-2-ad_cli">Actualizar</button>
                         </div>
                     </form>
                 </div>
@@ -117,6 +145,7 @@ $d = $c->get_data_by_id($_SESSION['user']);
     <script src="js/customs.js"></script>
     <script src="js/dashboard.js"></script>
     <script src="js/update_store.js"></script>
+    <script src="js/profile-client.js"></script>
 </body>
 
 </html>
