c5cd15fae895539762c6f86a64bf6fc5062e22b5
Ttito19 <andrezchavez.2000@gmail.com>
2020-12-13 10:18:42 -0500

    changes

--- shop/update_product.php
+++ shop/update_product.php
@@ -40,27 +40,32 @@ if(!isset($_GET['id']) || !is_numeric($_GET['id']) || $_GET['id'] == ""){
             <?php require_once 'includes/header-top.php'; ?>
             <div class="content-top">
                 <div class="content-title">
-                    <h1 class="title-dashboard lang_ttrq" key="title-top-update-products-cli-ad_cli">Actualiza tu producto</h1>
+                    <h1 class="title-dashboard lang_ttrq" key="title-top-update-products-cli-ad_cli">Actualiza tu
+                        producto</h1>
                 </div>
             </div>
             <div class="content-add-product">
-                <form action="" class="form-product" method="POST" autocomplete="false" id="form-product" enctype="multipart/form-data">
+                <form action="" class="form-product" method="POST" autocomplete="false" id="form-product"
+                    enctype="multipart/form-data">
                     <!--------- DATOS GENERALES DEL PRODUCTO --------->
                     <div class="product-content">
-                        <div class="product-head lang_ttrq" key="subtitle-top-update-products-cli-ad_cli">Información básica</div>
+                        <div class="product-head lang_ttrq" key="subtitle-top-update-products-cli-ad_cli">Información
+                            básica</div>
                         <div class="form-content">
                             <input type="hidden" name="prod" id="prod" value="<?php echo $_GET['id']; ?>">
                             <!-- input para traer el id de la tienda -->
                             <!--  NOMBRE DEL PRODUCTO-->
                             <div class="product-controls">
-                                <label for="" class="label-product lang_ttrq" key="txt-form-update_product-1">Nombre del producto</label>
+                                <label for="" class="label-product lang_ttrq" key="txt-form-update_product-1">Nombre del
+                                    producto</label>
                                 <input type="text" name="name" class="input-product" id="name">
                             </div>
                             <!--  CATEGORÍA DEL PRODUCTO -->
                             <div class="product-controls">
 
-                                <label for="" class="label-product lang_ttrq" key="txt-form-update_product-2">Categoría del producto</label>
-                                <select class="input-product" id="" name="categoria">
+                                <label for="" class="label-product lang_ttrq" key="txt-form-update_product-2">Categoría
+                                    del producto</label>
+                                <select class="input-product" name="categoria" id="list_categories">
                                     <?php
 
                                     $cate = $cat->get_categorias_tienda($tienda);
@@ -72,7 +77,8 @@ if(!isset($_GET['id']) || !is_numeric($_GET['id']) || $_GET['id'] == ""){
                             </div>
                             <!--  MARCA DEL PRODUCTO -->
                             <div class="product-controls">
-                                <label for="" class="label-product lang_ttrq" key="txt-form-update_product-3">Marcas</label>
+                                <label for="" class="label-product lang_ttrq"
+                                    key="txt-form-update_product-3">Marcas</label>
                                 <select name="marca" class="input-product" id="">
                                     <?php
 
@@ -86,9 +92,12 @@ if(!isset($_GET['id']) || !is_numeric($_GET['id']) || $_GET['id'] == ""){
 
                             <!--  PAÍS DEL PRODUCTO -->
                             <div class="product-controls">
-                                <label for="" class="label-product lang_ttrq" key="txt-form-update_product-4">Pais</label>
-                                <select name="pais" class="input-product select-onehidden" id="pais" value="Selecciona el País">
-                                    <option value="<?php echo $d[0]['id_pais']; ?>"><?php echo $d[0]['nombre_pais']; ?></option>
+                                <label for="" class="label-product lang_ttrq"
+                                    key="txt-form-update_product-4">Pais</label>
+                                <select name="pais" class="input-product select-onehidden" id="pais"
+                                    value="Selecciona el País">
+                                    <option value="<?php echo $d[0]['id_pais']; ?>"><?php echo $d[0]['nombre_pais']; ?>
+                                    </option>
                                     <?php
 
                                     foreach ($pais as $value) {
@@ -99,13 +108,15 @@ if(!isset($_GET['id']) || !is_numeric($_GET['id']) || $_GET['id'] == ""){
                             </div>
                             <!--  STOCK DEL PRODUCTO -->
                             <div class="product-controls">
-                                <label for="" class="label-product lang_ttrq" key="txt-form-update_product-5">Stock</label>
+                                <label for="" class="label-product lang_ttrq"
+                                    key="txt-form-update_product-5">Stock</label>
                                 <input type="number" name="stock" id="stock" class="input-product">
                             </div>
                             <!--  PRECIO DEL PRODUCTO -->
                             <div class="form-controls">
                                 <div class="product-controls-precio">
-                                    <label for="" class="label-product-precio lang_ttrq" key="txt-form-update_product-6">Precio</label>
+                                    <label for="" class="label-product-precio lang_ttrq"
+                                        key="txt-form-update_product-6">Precio</label>
                                     <input type="text" name="precio" class="input-product-precio" id="precio">
                                 </div>
                             </div>
@@ -116,7 +127,8 @@ if(!isset($_GET['id']) || !is_numeric($_GET['id']) || $_GET['id'] == ""){
                         <div class="form-content">
                             <div class="product-controls-imgs">
                                 <div class="content-principal-img_product">
-                                    <label for="" class="label-product-imgs lang_ttrq" key="txt-form-update_product-7">Imágenes principales</label>
+                                    <label for="" class="label-product-imgs lang_ttrq"
+                                        key="txt-form-update_product-7">Imágenes principales</label>
                                     <div class="add-imgs-primary">
                                         <input type="file" name="imagen" class="input-product-imgs imgs" id="img">
                                         <input type="hidden" name="imagen" class="input-product-imgs" id="imgitp">
@@ -140,12 +152,14 @@ if(!isset($_GET['id']) || !is_numeric($_GET['id']) || $_GET['id'] == ""){
                     <div class="product-content">
                         <div class="form-content">
                             <div class="product-controls-desc">
-                                <label for="" class="label-product-desc lang_ttrq" key="txt-form-update_product-8">Descripción</label>
+                                <label for="" class="label-product-desc lang_ttrq"
+                                    key="txt-form-update_product-8">Descripción</label>
                                 <textarea id="ckeditor" name="desc" class="ckeditor" cols="30" rows="10"></textarea>
                             </div>
                         </div>
                     </div>
-                    <button type="submit" class="btn-add-product lang_ttrq" id="btn-product-update" key="btn-form-update-product">Actualizar</button>
+                    <button type="submit" class="btn-add-product lang_ttrq" id="btn-product-update"
+                        key="btn-form-update-product">Actualizar</button>
                 </form>
             </div>
         </div>
