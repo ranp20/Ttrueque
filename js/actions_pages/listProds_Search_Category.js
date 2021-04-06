e2d50a64bd88c3c26de8300f370375b0b7380643
ranp20 <polancoranp2019@gmail.com>
2021-04-06 13:44:30 -0500

    add styles into message any products

--- js/actions_pages/listProds_Search_Category.js
+++ js/actions_pages/listProds_Search_Category.js
@@ -19,8 +19,8 @@ $(document).ready(function () {
       $("#list_all-products-search_category").append(`
         <div class="content-msg-any-prods_by_store">
          <div>
-           <i class="far fa-store-slash fa-4x"></i>
            <img src='./img/iconos_home/index-any-products.svg' alt=''>
+           <h1>Aún no hay productos disponibles para esta categoría</h1>
            <a href="allcategories">Ver todas las categorías</a>
          </div>
        </div>
