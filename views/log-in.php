7cd9b9e083b9a30a0a44e9b1dc094594cd4e614f
ranp20 <polancoranp2019@gmail.com>
2022-07-18 09:48:20 -0500

    repair and best performance of pages

--- views/log-in.php
+++ views/log-in.php
@@ -1,25 +1,21 @@
 <?php
 //COMPRIMIR ARCHIVOS DE TEXTO...
 (substr_count($_SERVER["HTTP_ACCEPT_ENCODING"], "gzip")) ? ob_start("ob_gzhandler") : ob_start();
-
 session_start();
-
 require_once '../php/class/all.php';
 $all = new ALl();
 $mante = $all->get_mantenience();
 $mantenience =  $mante[0]['state_mantenience'];
-
 if($mantenience == 'YES' || $mantenience == 'yes'){
-    header('Location: mantenience');
-}
-
-if (isset($_SESSION["user"])) {
-	header('location: ./');
+  header('Location: mantenience');
+}else{
+  if(isset($_SESSION["user"])){
+  	header('location: ./');
+  }
 }
 require_once '../php/class/all.php';
 $all = new All();
 $paises = $all->get_name_country();
-
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -45,48 +41,43 @@ $paises = $all->get_name_country();
 	$id = end($estado_cadena);
 	$estado =	strstr(empty($_GET["link"]) ? "" : $_GET["link"], '-' . $id, true);
 	?>
-  <div class="container-log-in-u">
-    <div class="content-log-in-u">
-      <section class="sec-log-in-u">
-        <header class="head-log-in-u">
-          <div class="logo-head-log-in-u">
-            <img src="img/logo/Login_Web-T-blue.svg" alt="logo_Ttrueque" width="190" height="70">
+  <div class="c-sectMain box-redx">
+    <section class="c-sectMain__cLogg">
+      <div class="c-sectMain__cLogg__cLogo">
+        <img src="img/logo/Login_Web-T-blue.svg" alt="logo_Ttrueque" class="img-fluid" width="100" height="100">
+      </div>
+      <div id="alert"></div>
+      <form class="c-sectMain__cLogg__cFrm" method="POST" id="frm-loginUserTtrq">
+        <input type="hidden" name="id" id="id" value="<?php echo empty($id) ? "" : $id;?>">
+        <input type="hidden" name="estado" id="estado" value="<?php echo empty($estado) ? "" : $estado;?>">
+        <div class="c-sectMain__cLogg__cFrm__cCtrls">
+          <div class="c-sectMain__cLogg__cFrm__cCtrls__cIcon">
+            <img src="img/iconos_home/home-login-mail.svg" alt="">
           </div>
-        </header>
-        <div id="alert"></div>
-        <form method="POST" id="frm-loginUserTtrq">
-          <input type="hidden" name="id" id="id" value="<?php echo empty($id) ? "" : $id ?>">
-          <input type="hidden" name="estado" id="estado" value="<?php echo empty($estado) ? "" : $estado  ?>">
-          <div class="cont-controls-login">
-            <div class="cont-icon-login">
-              <img src="img/iconos_home/home-login-mail.svg" alt="">
-            </div>
-            <input type="email" required maxlength="200" name="email" id="email" placeholder="Correo electrónico o número de celular">
-          </div>
-          <div class="cont-controls-login">
-            <div class="cont-icon-login">
-              <img src="img/iconos_home/home-login-lock.svg" alt="">
-            </div>
-            <input type="password" required maxlength="40" name="pass" id="password_in" placeholder="Ingrese su contraseña">
-          </div>
-          <a href="recuperar-contrasena" class="link-recoverypass">¿Ha olvidado su contraseña?</a>
-          <button type="submit">
-            <span>Iniciar Sesión</span>
-            <span>
-            </span>
-          </button>
-        </form>
-        <div class="content-register-log-in-u">
-          <div class="content-divisor-log-in-u">
-            <span>o</span>
-          </div>
-          <a role="button" href="account" class="btn-reg-log-in-u">Registrarse</a>
+          <input type="email" class="c-sectMain__cLogg__cFrm__cCtrls__ipt" required maxlength="200" name="email" id="email" placeholder="Correo electrónico o número de celular">
         </div>
-      </section>
-    </div>
+        <div class="c-sectMain__cLogg__cFrm__cCtrls">
+          <div class="c-sectMain__cLogg__cFrm__cCtrls__cIcon">
+            <img src="img/iconos_home/home-login-lock.svg" alt="">
+          </div>
+          <input type="password" class="c-sectMain__cLogg__cFrm__cCtrls__ipt" required maxlength="40" name="pass" id="password_in" placeholder="Ingrese su contraseña">
+        </div>
+        <a href="recuperar-contrasena" class="c-sectMain__cLogg__cFrm__linkrecovery">¿Ha olvidado su contraseña?</a>
+        <button type="submit">
+          <span>Iniciar Sesión</span>
+          <span></span>
+        </button>
+      </form>
+      <div class="c-sectMain__cLogg__cReg">
+        <div class="c-sectMain__cLogg__cReg__hr">
+          <span>o</span>
+        </div>
+        <a role="button" href="account" class="c-sectMain__cLogg__cReg__link">Registrarse</a>
+      </div>
+    </section>
   </div>
   <div id="cont-AlertMssgTtrqUsr"></div>
-  <script async src="<?= $url ?>js/actions_pages/login.js"></script>
-  <script async src="<?= $url ?>js/actions_pages/account.js"></script>
+  <script async src="<?= $url;?>js/actions_pages/login.js"></script>
+  <script async src="<?= $url;?>js/actions_pages/account.js"></script>
 </body>
 </html>
