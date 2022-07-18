0cfed99d2e9cc7c4ea8a2f51cd59900c1a03903c
ranp20 <polancoranp2019@gmail.com>
2022-07-18 10:21:02 -0500

    repair and best performance of pages 5

--- view_home/header_links-home.php
+++ view_home/header_links-home.php
@@ -2,11 +2,30 @@
   $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]";
   $url =  $actual_link . "/" ."Ttrueque/";
 ?>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-<meta name="description" content="">
-<meta name="author" content="Ttrueque">
+<meta charset="UTF-8"/>
+<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate"/>
+<meta http-equiv="Pragma" content="no-cache"/>
+<meta http-equiv="Expires" content="0"/>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, minimum-scale=1.0, shrink-to-fit=no, viewport-fit=cover"/>
+<meta name="keywords" content="ttrueque, comprar y vender, dinero convencional, comprar y vender en forma segura"/>
+<meta name="description" content="Ttrueque te facilita para comprar y vender en forma SEGURA, y sin usar ni arriesgar tu DINERO CONVENCIONAL."/>
+<meta name="theme-color" content="#003857"/>
+<meta name="author" content="R@np-2021"/>
+<meta name="robots" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1"/>
+<meta name="twitter.card" content="summary"/>
+<meta property="og:locale" content="es_ES"/>
+<meta property="og:type" content="website"/>
+<meta property="og:site_name" content="Ttrueque"/>
+<meta property="og:url" name="twitter.url" content="https://localhost/Ttrueque"/>
+<meta property="og:title" name="twitter.title" content="Ttrueque"/>
+<meta property="og:description" name="twitter.description" content="Ttrueque te facilita para comprar y vender en forma SEGURA, y sin usar ni arriesgar tu DINERO CONVENCIONAL."/>
+<!-- <meta property="og:image" name="twitter.image" content="./views/assets/img/logos/logotipo-camel.png"/>
+<link rel="icon" type="image/x-icon" href="./views/assets/img/favicon/favicon-camel.png"/>
+<link rel="apple-touch-icon" href="./views/assets/img/favicon/favicon-camel.png"/> -->
+<link rel="canonical" href="https://localhost/Ttrueque"/>
+<!-- CUSTOMS CSS -->
 <link href="./css/style.css" rel="stylesheet">
 <link href="./css/estilos.css" rel="stylesheet">
 <link href="./css/customs/custom.css" rel="stylesheet">
