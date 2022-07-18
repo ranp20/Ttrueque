ccaec1b88956e2a6b06b7419d9e4dab94585f8e1
ranp20 <polancoranp2019@gmail.com>
2022-07-17 22:05:22 -0500

    repair performance of code 1

--- view_home/presentacion_texto_one_b.php
+++ view_home/presentacion_texto_one_b.php
@@ -1,3 +1,8 @@
+<?php
+//COMPRIMIR ARCHIVOS DE TEXTO...
+(substr_count($_SERVER["HTTP_ACCEPT_ENCODING"], "gzip")) ? ob_start("ob_gzhandler") : ob_start();
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -6,50 +11,50 @@
 </head>
 <body>
 	<?php require_once './presentacion_header_texto_b.php'; ?>
-	<div class="cont__txtone">
-		<section class="cont__txtone--cont">
-			<h2 class="cont__txtone--cont--title">Es gratuito abrir una cuenta en Ttrueque</h5>
-			<div class="cont__txtone--cont--cards">
-				<div class="cont__txtone--cont--cards--item">
-					<div class="cont__txtone--cont--cards--item--cImg">
-						<img loading="lazy" class="cont__txtone--cont--cards--item--cImg--img img-fluid" src="img/banners/enterprise-team-glory.png" alt="">
+	<div class="c-sectMain box-redx">
+		<section class="c-sectMain__c">
+			<h2 class="c-sectMain__c__title">Es gratuito abrir una cuenta en Ttrueque</h5>
+			<div class="c-sectMain__c__cCards">
+				<div class="c-sectMain__c__cCards__i">
+					<div class="c-sectMain__c__cCards__i__cIcon">
+						<img loading="lazy" src="img/banners/enterprise-team-glory.png" alt="icon_banner_page-one" class="img-fluid" width="100" height="100">
 					</div>
-					<div class="cont__txtone--cont--cards--item--cInfo">
-						<h3 class="cont__txtone--cont--cards--item--cInfo--type">Empresa</h3>
-						<p class="cont__txtone--cont--cards--item--cInfo--desc">Vende y Compra sin Dinero.</p>
-						<a href="account" class="cont__txtone--cont--cards--item--cInfo--link">Registrate sin costo</a>
+					<div class="c-sectMain__c__cCards__i__cDesc">
+						<h3>Empresa</h3>
+						<p>Vende y Compra sin Dinero.</p>
+						<a href="account" title="Registrarse">Registrate sin costo</a>
 					</div>
 				</div>
-				<div class="cont__txtone--cont--cards--item">
-					<div class="cont__txtone--cont--cards--item--cImg">
-						<img loading="lazy" class="cont__txtone--cont--cards--item--cImg--img img-fluid" src="img/banners/women-bussiness-happy.png" alt="">
+				<div class="c-sectMain__c__cCards__i">
+					<div class="c-sectMain__c__cCards__i__cIcon">
+						<img loading="lazy" src="img/banners/women-bussiness-happy.png" alt="icon_banner_page-one" class="img-fluid" width="100" height="100">
 					</div>
-					<div class="cont__txtone--cont--cards--item--cInfo">
-						<h3 class="cont__txtone--cont--cards--item--cInfo--type">Personal</h3>
-						<p class="cont__txtone--cont--cards--item--cInfo--desc">Vende y Compra sin Dinero.</p>
-						<a href="account" class="cont__txtone--cont--cards--item--cInfo--link">Registrate sin costo</a>
+					<div class="c-sectMain__c__cCards__i__cDesc">
+						<h3>Personal</h3>
+						<p>Vende y Compra sin Dinero.</p>
+						<a href="account" title="Registrarse">Registrate sin costo</a>
 					</div>
 				</div>
 			</div>
 		</section>
-		<section class="cont__txtone--cont">
-			<h2 class="cont__txtone--cont--title">NOTABLES BENEFICIOS</h2>
-			<div class="cont__txtone--cont--CItems">
-				<div class="cont__txtone--cont--CItems--item">
-					<h3 class="cont__txtone--cont--CItems--item--title">AL VENDER</h3>
-					<div class="cont__txtone--cont--CItems--item--textimg">
-						<p class="cont__txtone--cont--CItems--item--textimg--desc">Tu ganancia se incrementa automáticamente. El monto de tu venta se registrará en tu cuenta con el 20% adicional.</p>
-						<div class="cont__txtone--cont--CItems--item--textimg--cImg">
-							<img loading="lazy" class="cont__txtone--cont--CItems--item--textimg--cImg--img img-fluid"src="img/Utilities/carrito_cdr_ttrq.jpg" alt="backgr_img_ttrq_01">
+		<section class="c-sectMain__c">
+			<h2 class="c-sectMain__c__title">NOTABLES BENEFICIOS</h2>
+			<div class="c-sectMain__c__cflx">
+				<div class="c-sectMain__c__cflx__i">
+					<h3 class="c-sectMain__c__cflx__i__title">AL VENDER</h3>
+					<div class="c-sectMain__c__cflx__i__cDesc">
+						<p>Tu ganancia se incrementa automáticamente. El monto de tu venta se registrará en tu cuenta con el 20% adicional.</p>
+						<div class="c-sectMain__c__cflx__i__cDesc__cIcon">
+							<img loading="lazy" src="img/Utilities/carrito_cdr_ttrq.jpg" alt="backgr_img_ttrq_01" class="img-fluid" width="100" height="100">
 						</div>
 					</div>
 				</div>
-				<div class="cont__txtone--cont--CItems--item">
-					<h3 class="cont__txtone--cont--CItems--item--title">AL COMPRAR</h3>
-					<div class="cont__txtone--cont--CItems--item--textimg">
-						<p class="cont__txtone--cont--CItems--item--textimg--desc">Cuando compres en los establecimientos	fisicos podrás usar el aplicativo QR (en tu celular) o la targeta Ttrueque, y en las tiendas virtuales, tu cuenta Ttrueque en línea.</p>
-						<div class="cont__txtone--cont--CItems--item--textimg--cImg">
-							<img loading="lazy" class="cont__txtone--cont--CItems--item--textimg--cImg--img img-fluid" src="img/Utilities/boleto_cdr_ttrq.jpg" alt="backgr_img_ttrq_02">
+				<div class="c-sectMain__c__cflx__i">
+					<h3 class="c-sectMain__c__cflx__i__title">AL COMPRAR</h3>
+					<div class="c-sectMain__c__cflx__i__cDesc">
+						<p>Cuando compres en los establecimientos	fisicos podrás usar el aplicativo QR (en tu celular) o la targeta Ttrueque, y en las tiendas virtuales, tu cuenta Ttrueque en línea.</p>
+						<div class="c-sectMain__c__cflx__i__cDesc__cIcon">
+							<img loading="lazy" src="img/Utilities/boleto_cdr_ttrq.jpg" alt="backgr_img_ttrq_02" class="img-fluid" width="100" height="100">
 						</div>
 					</div>
 				</div>
@@ -57,6 +62,5 @@
 		</section>
 	</div>
 	<div id="toTopgobtn"></div>
-	<script src="js/main.js"></script>
 </body>
 </html>
