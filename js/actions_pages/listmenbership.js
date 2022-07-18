f3661c63c5db89df474cc83b4275115252157fe6
ranp20 <polancoranp2019@gmail.com>
2022-07-17 19:58:48 -0500

    repair styles and total performance 6

--- js/actions_pages/listmenbership.js
+++ js/actions_pages/listmenbership.js
@@ -1,30 +1,64 @@
-$(function(){
+$(() => {
+  listMemberships();
+});
+$(document).ready(function() {
+  var estado = false;
+  $('#b-two-t-m-xd').css({
+    "overflow": "auto"
+  });
+  $('#btn-slide-info').on('mouseover', function() {
+    $('.c-sectMain').slideToggle();
+    if(estado == true){
+      $(this).text("Mayores Detalles sobre Membresía");
+      $('#b-two-t-m-xd').css({
+        "overflow": "auto"
+      });
+      $('#main-membership-info').css({
+        "display": "none"
+      });
+      estado = false;
+    }else{
+      $(this).text('');
+      $('#b-two-t-m-xd').css({
+        "overflow": "auto"
+      });
+      $('#main-membership-info').css({
+        "display": "block",
+        "margin-top": "-27px"
+      });
+      estado = true;
+    }
+  });
+});
+function listMemberships(){
   $.ajax({
     url: "./php/list_menbership.php",
     dataType: "JSON",
     method: "POST",
     contentType: 'application/x-www-form-urlencoded;charset=UTF-8',
-  }).done(function (res) {
-
-    $.each(res, function (i, v) {
-      var img_path = "./admin/images/menbresia/" + v.image;
-      var a = $("<div />").html((v.descripcion).replace(/</g, '&lt;').replace(/>/g, '&gt;')).text();
-
-      $("#targets-info-menbershi").append(`
-          <li class="cont__tblpricetxttwo--info--mtable--item">
-            <div class="cont__tblpricetxttwo--info--mtable--item--cImg">
-              <img loading="lazy" class="cont__tblpricetxttwo--info--mtable--item--cImg--img img-fluid" src="${img_path}">
+  }).done((e) => {
+    if(e != "" && e != "[]"){
+      $.each(e, function(i, v){
+        let img_path = "./admin/images/menbresia/" + v.image;
+        let a = $("<div />").html((v.descripcion).replace(/</g, '&lt;').replace(/>/g, '&gt;')).text();
+        $("#targets-info-menbershi").append(`
+          <li class="c-tblpriceSlideUp__c__m__i">
+            <div class="c-tblpriceSlideUp__c__m__i__cIcon">
+              <img loading="lazy" class="img-fluid" src="${img_path}" alt="membresia_de_${v.image}" width="100" height="100">
               </div>
             </div>
-            <div class="cont__tblpricetxttwo--info--mtable--item--cBenefits">
+            <div class="c-tblpriceSlideUp__c__m__i__cDesc">
               <h4>${v.tipo}</h4>
-              <div class="cont__tblpricetxttwo--info--mtable--item--cBenefits--cont">
+              <div class="c-tblpriceSlideUp__c__m__i__cDesc__cont">
                 ${a}
               </div>
             </div>
-            <a href="cliente/menbresia" class="cont__tblpricetxttwo--info--mtable--item--cBenefits--link">Conoce más</a>
+            <a href="cliente/menbresia" class="c-tblpriceSlideUp__c__m__i__cDesc__link" title="Conoce más">Conoce más</a>
           </li>
         `);
-    });
+      });
+    }else{
+      console.log('Lo sentimos, hubo un error al procesar la información.');
+    }
   });
-});
+}
--- view_home/presentacion_texto_two_b.php
+++ view_home/presentacion_texto_two_b.php
@@ -1,3 +1,8 @@
+<?php
+//COMPRIMIR ARCHIVOS DE TEXTO...
+(substr_count($_SERVER["HTTP_ACCEPT_ENCODING"], "gzip")) ? ob_start("ob_gzhandler") : ob_start();
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -5,103 +10,71 @@
   <?php require_once 'header_links-home.php';?>
 </head>
 <body>
-	<div id="page" class="page-two_b">
+	<div>
 		<?php require_once 'presentacion_header_texto_b.php'; ?>
-		<div class="cont__txttwo">
-			<section class="cont__txttwo--cont">
-				<div class="cont__txttwo--cont--mobileresp">
-					<h3 class="cont__txttwo--cont--mobileresp--title">ADQUIERE E INTERCAMBIA CON BIKKERS</h3>
-					<h3 class="cont__txttwo--cont--mobileresp--subtitle">Tres pasos para que adquieras Bikkers y compres o vendas en establecimientos físicos o en línea:</h3>
-					<div class="cont__txttwo--cont--mobileresp--cSteps">
-						<div class="cont__txttwo--cont--mobileresp--cSteps--step">
-							<p class="cont__txttwo--cont--mobileresp--cSteps--step--text"><b>1.- </b> Regístrate gratuitamente en Ttrueque, en la Web www.ttrueque.com o en el aplicativo Ttrueque que puedes descargar de Play Store.</p>
-							<img loading="lazy" class="cont__txttwo--cont--mobileresp--cSteps--step--img img-fluid" src="img/Utilities/img_txt2_1.jpg">
+		<div class="c-sectMain box-redx">
+			<section class="c-sectMain__cMobDesk">
+				<div class="c-sectMain__cMobDesk__cMob">
+					<h3 class="c-sectMain__cMobDesk__cMob__title">ADQUIERE E INTERCAMBIA CON BIKKERS</h3>
+					<h3 class="c-sectMain__cMobDesk__cMob__subtitle">Tres pasos para que adquieras Bikkers y compres o vendas en establecimientos físicos o en línea:</h3>
+					<div class="c-sectMain__cMobDesk__cMob__cSteps">
+						<div class="c-sectMain__cMobDesk__cMob__cSteps__i">
+							<p><strong>1.- </strong> Regístrate gratuitamente en Ttrueque, en la Web www.ttrueque.com o en el aplicativo Ttrueque que puedes descargar de Play Store.</p>
+							<img loading="lazy" src="img/Utilities/img_txt2_1.jpg" alt="icon_descriptive_page-two-mobile" class="img-fluid" width="100" height="100">
 						</div>
-						<div class="cont__txttwo--cont--mobileresp--cSteps--step">
-							<p class="cont__txttwo--cont--mobileresp--cSteps--step--text"><b>2.- </b> Coloca los productos en la sección TIENDAS de la plataforma para que los usuarios conozcan lo que ofreces. Dispondrás de una tienda gratuita.</p>
-							<img loading="lazy" class="cont__txttwo--cont--mobileresp--cSteps--step--img img-fluid" src="img/Utilities/img_txt2_2.jpg">
+						<div class="c-sectMain__cMobDesk__cMob__cSteps__i">
+							<p><strong>2.- </strong> Coloca los productos en la sección TIENDAS de la plataforma para que los usuarios conozcan lo que ofreces. Dispondrás de una tienda gratuita.</p>
+							<img loading="lazy" src="img/Utilities/img_txt2_2.jpg" alt="icon_descriptive_page-two-mobile" class="img-fluid" width="100" height="100">
 						</div>
-						<div class="cont__txttwo--cont--mobileresp--cSteps--step">
-							<p class="cont__txttwo--cont--mobileresp--cSteps--step--text"><b>3.- </b> Los usuarios adquirirán tus productos usando Bikkers, y al mismo tiempo en tu cuenta se registrará tu saldo acrededor en Bikkers.</p>
-							<img loading="lazy" class="cont__txttwo--cont--mobileresp--cSteps--step--img img-fluid" src="img/Utilities/img_txt2_3.jpg">
+						<div class="c-sectMain__cMobDesk__cMob__cSteps__i">
+							<p><strong>3.- </strong> Los usuarios adquirirán tus productos usando Bikkers, y al mismo tiempo en tu cuenta se registrará tu saldo acrededor en Bikkers.</p>
+							<img loading="lazy" src="img/Utilities/img_txt2_3.jpg" alt="icon_descriptive_page-two-mobile" class="img-fluid" width="100" height="100">
 						</div>
 					</div>
 				</div>
-				<div class="cont__txttwo--cont--desktopresp">
-					<div class="cont__txttwo--cont--desktopresp--one">
-						<h3 class="cont__txttwo--cont--desktopresp--one--title">ADQUIERE E INTERCAMBIA CON BIKKERS</h3>
+				<div class="c-sectMain__cMobDesk__cDesk">
+					<div class="c-sectMain__cMobDesk__cDesk__i">
+						<h3 class="c-sectMain__cMobDesk__cDesk__i__title">ADQUIERE E INTERCAMBIA CON BIKKERS</h3>
 					</div>
-					<div class="cont__txttwo--cont--desktopresp--two">
-						<h3 class="cont__txttwo--cont--desktopresp--two--title">Tres pasos para que adquieras Bikkers y compres o vendas en establecimientos físicos o en línea:</h3>
-						<ul class="cont__txttwo--cont--desktopresp--two--msteps">
-							<li class="cont__txttwo--cont--desktopresp--two--msteps--item">
-								<p class="cont__txttwo--cont--desktopresp--two--msteps--item--text"><b>1.- </b> Regístrate gratuitamente en Ttrueque, en la Web www.ttrueque.com o en el aplicativo Ttrueque que puedes descargar de Play Store.</p>
+					<div class="c-sectMain__cMobDesk__cDesk__i">
+						<h3 class="c-sectMain__cMobDesk__cDesk__i__title">Tres pasos para que adquieras Bikkers y compres o vendas en establecimientos físicos o en línea:</h3>
+						<ul>
+							<li>
+								<p><strong>1.- </strong> Regístrate gratuitamente en Ttrueque, en la Web www.ttrueque.com o en el aplicativo Ttrueque que puedes descargar de Play Store.</p>
 							</li>
-							<li class="cont__txttwo--cont--desktopresp--two--msteps--item">
-								<p class="cont__txttwo--cont--desktopresp--two--msteps--item--text"><b>2.- </b> Coloca los productos en la sección TIENDAS de la plataforma para que los usuarios conozcan lo que ofreces. Dispondrás de una tienda gratuita.</p>
+							<li>
+								<p><strong>2.- </strong> Coloca los productos en la sección TIENDAS de la plataforma para que los usuarios conozcan lo que ofreces. Dispondrás de una tienda gratuita.</p>
 							</li>
-							<li class="cont__txttwo--cont--desktopresp--two--msteps--item">
-								<p class="cont__txttwo--cont--desktopresp--two--msteps--item--text"><b>3.- </b> Los usuarios adquirirán tus productos usando Bikkers, y al mismo tiempo en tu cuenta se registrará tu saldo acrededor en Bikkers.</p>
+							<li>
+								<p><strong>3.- </strong> Los usuarios adquirirán tus productos usando Bikkers, y al mismo tiempo en tu cuenta se registrará tu saldo acrededor en Bikkers.</p>
 							</li>
 						</ul>
 					</div>
-					<div class="cont__txttwo--cont--desktopresp--three">
-						<ul class="cont__txttwo--cont--desktopresp--three--msteps">
-							<li class="cont__txttwo--cont--desktopresp--three--msteps--item">
-								<img loading="lazy" class="cont__txttwo--cont--desktopresp--three--msteps--item--img img-fluid" src="img/Utilities/img_txt2_1.jpg">
+					<div class="c-sectMain__cMobDesk__cDesk__i">
+						<ul>
+							<li>
+								<img loading="lazy" src="img/Utilities/img_txt2_1.jpg" alt="icon_descriptive_page-two-desktop" class="img-fluid" width="100" height="100">
 							</li>
-							<li class="cont__txttwo--cont--desktopresp--three--msteps--item">
-								<img loading="lazy" class="cont__txttwo--cont--desktopresp--three--msteps--item--img img-fluid" src="img/Utilities/img_txt2_2.jpg">
+							<li>
+								<img loading="lazy" src="img/Utilities/img_txt2_2.jpg" alt="icon_descriptive_page-two-desktop" class="img-fluid" width="100" height="100">
 							</li>
-							<li class="cont__txttwo--cont--desktopresp--three--msteps--item">
-								<img loading="lazy" class="cont__txttwo--cont--desktopresp--three--msteps--item--img img-fluid" src="img/Utilities/img_txt2_3.jpg">
+							<li>
+								<img loading="lazy" src="img/Utilities/img_txt2_3.jpg" alt="icon_descriptive_page-two-desktop" class="img-fluid" width="100" height="100">
 							</li>
 						</ul>
 					</div>
 				</div>
 			</section>
 		</div>
-		<a href="javascript:void(0);" id="btn-slide-info" class="btn-slideinfo__txttwo">Mayores detalles sobre Membresía</a>
-		<div class="cont__tblpricetxttwo" id="main-membership-info">
-			<div class="cont__tblpricetxttwo--info">
-				<h3 class="cont__tblpricetxttwo--info--title">MEJORA TUS BENEFICIOS</h3>
-				<ul class="cont__tblpricetxttwo--info--mtable" id="targets-info-menbershi"></ul>
+		<a href="javascript:void(0);" class="c-blck-linkSlideUp" id="btn-slide-info" title="Mayores detalles sobre membresías">Mayores detalles sobre Membresía</a>
+		<div class="c-tblpriceSlideUp" id="main-membership-info">
+			<div class="c-tblpriceSlideUp__c box-redx">
+				<h3 class="c-tblpriceSlideUp__c__title">MEJORA TUS BENEFICIOS</h3>
+				<ul class="c-tblpriceSlideUp__c__m" id="targets-info-menbershi"></ul>
 			</div>
 		</div>
 	</div>
 	<div id="toTopgobtn"></div>
-	<script src="js/main.js"></script>
 	<script src="./js/actions_pages/listmenbership.js"></script>
-	<script type="text/javascript">
-		$(document).ready(function() {
-			var estado = false;
-			$('#b-two-t-m-xd').css({
-				"overflow": "auto"
-			});
-			$('#btn-slide-info').on('mouseover', function() {
-				$('.cont__txttwo').slideToggle();
-				if (estado == true) {
-					$(this).text("Mayores Detalles sobre Membresía");
-					$('#b-two-t-m-xd').css({
-						"overflow": "auto"
-					});
-					$('#main-membership-info').css({
-						"display": "none"
-					});
-					estado = false;
-				} else {
-					$(this).text('');
-					$('#b-two-t-m-xd').css({
-						"overflow": "auto"
-					});
-					$('#main-membership-info').css({
-						"display": "block",
-						"margin-top": "-27px"
-					});
-					estado = true;
-				}
-			});
-		});
-	</script>
 </body>
 </html>
