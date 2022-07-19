f56374a02d0003eb180c03800b2ec502982c4c23
ranp20 <polancoranp2019@gmail.com>
2022-07-19 18:31:35 -0500

    repair styles and files javascript into all project 7

--- admin/includes/header_links.php
+++ admin/includes/header_links.php
@@ -3,7 +3,8 @@ $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https"
 // CONFIGURACIÓN LOCALHOST
 $url =  $actual_link . "/Ttrueque/admin/";
 $urlCli =  $actual_link . "/" ."Ttrueque/";
-?>
+/*
+echo "
 <meta charset="UTF-8"/>
 <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
 <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
@@ -51,3 +52,51 @@ $urlCli =  $actual_link . "/" ."Ttrueque/";
 <link rel="stylesheet" href="css/customs/customs.css">
 <!-- FONTAWESOME CDN -->
 <script defer src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" integrity="sha384-rOA1PnstxnOBLzCLMcre8ybwbTmemjzdNlILg8O7z1lUkLXozs4DHonlDtnE7fpc" crossorigin="anonymous"></script>
+";
+*/
+?>
+<!-- ETIQUETAS PROVICIONALES - ELIMINAR LUEGO DE CAMBIAR LOS ÍCONOS -->
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+<meta name="description" content="">
+<meta name="author" content="Ttrueque">
+<meta name="theme-color" content="#3e454c">
+<!-- //Font awesome -->
+<link rel="stylesheet" href="css/font-awesome.min.css">
+<!-- //Bootstrap files CSS -->
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/dataTables.bootstrap.min.css">
+<link rel="stylesheet" href="css/bootstrap-social.css">
+<link rel="stylesheet" href="css/bootstrap-select.css">
+<link rel="stylesheet" href="css/fileinput.min.css">
+<link rel="stylesheet" href="css/awesome-bootstrap-checkbox.css">
+<!-- //style.css -->
+<link rel="stylesheet" href="css/style.css">
+<!-- //customs.css-->
+<link rel="stylesheet" href="css/customs/customs.css">
+<!---//JQUERY -->
+<script src="js/jquery.min.js"></script>
+<!-- //CKEDITOR SCRIPT -->
+<script src="../shop/ckeditor/ckeditor.js"></script>
+<!-- //BOOSTRAP CDN-->
+
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<!---------  CDN BOOTSTRAP  ------->
+<!-------   CSS ------------>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<!-------   JS ------------>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+</script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+</script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+    integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+</script>
+<!-- SWEEET ALERT 2-->
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+<!-----//ICON-PAGES------>
+<link rel="icon" href="../img/icon-pages/drawable-xxxhdpi-icon.png" type="image/ico">
