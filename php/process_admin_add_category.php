88b0aab0c9837c5a4d2857d66ab4f1d0d6f8a749
ranp20 <polancoranp2019@gmail.com>
2022-07-19 21:03:54 -0500

    repair styles and files javascript into all project 8

--- php/process_admin_add_category.php
+++ php/process_admin_add_category.php
@@ -1,57 +1,51 @@
 <?php 
-  session_start();
+session_start();
+if(isset($_POST['name']) && isset($_POST['submit'])){
+  require_once("class/categoria.php");
+  $c = new Categoria();
 
-  if(isset($_POST['name']) && isset($_POST['submit'])){
-      require_once("class/categoria.php");
-      $c = new Categoria();
-      
-      $_FILES['image'] = str_replace(" ", "-", $_FILES['image']);
-
-      $nombre = $_POST['name'];
-      $imagen = $_FILES['image'];
-      
-      if($_FILES['image']['error']>0){
-          $_SESSION["msg"] = "Error al cargar el archivo";
-      }else{
-          $admitido = array('image/png','image/gif','image/jpg','image/jpeg');
-          $limit_kb = 2000;
-          
-          if(in_array($_FILES['image']['type'],$admitido) && $_FILES['image']['size'] <= $limit_kb * 1024){
-              $path = '../admin/images/categoria/';
-              
-              $img_nombre = $_FILES['image']['name'];
-              $imagen_replace = str_replace(['%C3%A9','%C3%AD'], ['é', 'í'], $img_nombre);
-              
-              $img = $path.$imagen_replace;
-    
-              if(!file_exists($img)){
-
-                  $res = @move_uploaded_file($_FILES['image']['tmp_name'], $img);
-
-                  if($res){
-                      
-                      $r = $c->add_image($img);
-                      $_SESSION["msg"] = "Se guardó con éxito";
-                      header("Location: ../admin/manage-category.php");
-                      
-                  }else{
-                      $_SESSION["msg"] = "Error al guardar el archivo";    
-                  }
-
-              }else{
-                  $_SESSION["msg"] = "El archivo ya existe";
-              }
-          }else{
-              $_SESSION["msg"] = "Archivo no permitido o excede el tamaño";
-          }
-      }
-      
-      $response = $c->add_category($nombre,$imagen['name']);
-      
-  }
-  else{
-      $_SESSION['msg'] = 'Error al agregar uno a más registros';
-      header("Location: ../admin/add-category.php");
-  }
-    header("Location: ../admin/manage-category.php");
+  $_FILES['image'] = str_replace(" ", "-", $_FILES['image']);
+  $nombre = $_POST['name'];
+  $imagen = $_FILES['image'];
   
+  if($_FILES['image']['error']>0){
+    $_SESSION["msg"] = "Error al cargar el archivo";
+  }else{
+    $admitido = array('image/png','image/gif','image/jpg','image/jpeg');
+    $limit_kb = 2000;
+    
+    if(in_array($_FILES['image']['type'],$admitido) && $_FILES['image']['size'] <= $limit_kb * 1024){
+      $file_name = $_FILES['image']['name'];
+      $file_parts = pathinfo($file_name);
+      // $file_dirname = str_replace(['%C3%A9','%C3%AD'], ['é', 'í'], $file_parts['filename']);
+      $file_extension = $file_parts['extension'];
+      $file_new_name = "category_" . strtolower($_POST['name']);
+      // ENCRIPTACIÓN DE NOMBRE DE LA IMAGEN
+      $img = md5(base64_encode($file_new_name));
+      $file_lowercase = $img . "." . $file_extension;
+      $file_origin = $_FILES['image']['tmp_name'];
+      $file_folder = "../admin/images/categoria/";
+
+      if(!file_exists($file_lowercase)){
+        $res = @move_uploaded_file($file_origin, $file_folder . $file_lowercase);
+
+        if($res){
+          $r = $c->add_image($img);
+          $_SESSION["msg"] = "Se guardó con éxito";
+          header("Location: ../admin/manage-category.php");
+        }else{
+          $_SESSION["msg"] = "Error al guardar el archivo";    
+        }
+      }else{
+        $_SESSION["msg"] = "El archivo ya existe";
+      }
+    }else{
+      $_SESSION["msg"] = "Archivo no permitido o excede el tamaño";
+    }
+  }
+  $response = $c->add_category($nombre,$imagen['name']); 
+}
+else{
+  $_SESSION['msg'] = 'Error al agregar uno a más registros';
+  header("Location: ../admin/add-category.php");
+}
--- php/process_admin_update_category.php
+++ php/process_admin_update_category.php
@@ -1,45 +1,38 @@
 <?php
-
 session_start();
-
 require_once 'class/categoria.php';
 $c = new Categoria();
+if(isset($_POST['submit'])){
+  $nombre = $_POST['name'];
+  $image = $_FILES['image']['name'] == "" ? $_FILES['image'] : $_FILES['image']['name'];
+  $id = $_POST['id'];
 
-if (isset($_POST['submit'])) {
-    $nombre = $_POST['name'];
-    $image = $_FILES['image']['name'] == "" ? $_POST['image'] : $_FILES['image']['name'];
-    $id = $_POST['id'];
-
-    // if ($_FILES['image']['error'] > 0) {
-    //     echo 'Error al cargar el archivo';
-    // } else {
+  if($_FILES['image']['error']>0){
+    $_SESSION["msg"] = "Error al cargar el archivo";
+  }else{
     $admitido = array('image/png', 'image/gif', 'image/jpg', 'image/jpeg');
     $limit_kb = 2000;
 
-    if (in_array($_FILES['image']['type'], $admitido) && $_FILES['image']['size'] <= $limit_kb * 1024) {
-        $path = '../admin/images/categoria/';
-        $img = $path . $image;
+    if(in_array($_FILES['image']['type'], $admitido) && $_FILES['image']['size'] <= $limit_kb * 1024){
+      $file_name = $_FILES['image']['name'];
+      $file_parts = pathinfo($file_name);
+      // $file_dirname = str_replace(['%C3%A9','%C3%AD'], ['é', 'í'], $file_parts['filename']);
+      $file_extension = $file_parts['extension'];
+      $file_new_name = "category_" . strtolower($_POST['name']);
+      // ENCRIPTACIÓN DE NOMBRE DE LA IMAGEN
+      $img = md5(base64_encode($file_new_name));
+      $file_lowercase = $img . "." . $file_extension;
+      $file_origin = $_FILES['image']['tmp_name'];
+      $file_folder = "../admin/images/categoria/";
 
-        if (!file_exists($img)) {
-
-
-            $res = move_uploaded_file($_FILES['image']['tmp_name'], $img);
-
-            // if ($res) {
-            //     $_SESSION["msg"] = "Archivo guardado correctamente";
-            // } else {
-            //     echo 'Error al guardar';
-            // }
-
-        }
-    } else {
-        echo "Arhivo no permitido o excede el tamaño";
+      $res = @move_uploaded_file($file_origin, $file_folder . $file_lowercase);
+    }else{
+      echo "Arhivo no permitido o excede el tamaño";
     }
-    // }
-    // print_r($img);
-    // exit();
-    $res =   $c->update_ruta_image_category($image, $id);
-    $res = $c->update_category($nombre, $image, $id);
-
+    $res = $c->update_ruta_image_category($file_lowercase, $id);
+    $res = $c->update_category($nombre, $file_lowercase, $id);
     header('location: ../admin/manage-category.php');
+  }
+}else{
+  header('location: ../admin/manage-category.php');
 }
