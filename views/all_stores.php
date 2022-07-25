28a31a926d86335a3f93ef71fd91358dcb43a46d
ranp20 <polancoranp2019@gmail.com>
2022-07-24 21:56:38 -0500

    repair changes in all pages

--- views/all_stores.php
+++ views/all_stores.php
@@ -35,7 +35,9 @@ $dat = $c->select_tienda();
       <section class="list-stores-officials-ttrk-c">
         <ul class="items-str-off-ttrk">
             <?php
-							foreach ($dat as $val) {
+							foreach ($dat as $val){
+								$urlpath = $_SERVER['DOCUMENT_ROOT']."/Ttrueque/shop/images/store/".$val['logo'];
+								$urlpath_default = "../Ttrueque/shop/images/store/default-store.png";
 								if($val['logo'] == "default-store.png"){
 									echo "
 									<li class='item-str-off-into'>
@@ -44,7 +46,7 @@ $dat = $c->select_tienda();
 												<span style='box-sizing: border-box; display: block; width: initial; height: initial; background: none; opacity: 1; border: 0px; margin: 0px; padding: 0px; max-width: 100%;'>
 													<img alt='' aria-hidden='true' src='data:image/svg+xml,%3csvg%20xmlns=%27http://www.w3.org/2000/svg%27%20version=%271.1%27%20width=%27138%27%20height=%27138%27/%3e' style='display: block; max-width: 100%; width: initial; height: initial; background: none; opacity: 1; border: 0px; margin: 0px; padding: 0px;'>
 												</span>
-												<img src='../Ttrueque/shop/images/store/{$val['logo']}' alt='{$val['nombre_tienda']}' class='logo-off-mrkt-c-ttrk img-fluid' style='position: absolute;inset: 0px;box-sizing: border-box;padding: 0px;border: none;margin: auto;display: block;width: 0px;height: 0px;min-width: 80%;max-width: 80%;min-height: 80%;max-height: 80%;object-fit:cover;border-radius: 8px;vertical-align: top;' decoding='async'>
+												<img src='../Ttrueque/shop/images/store/default-store.png' alt='{$val['nombre_tienda']}' class='logo-off-mrkt-c-ttrk img-fluid' style='position: absolute;inset: 0px;box-sizing: border-box;padding: 0px;border: none;margin: auto;display: block;width: 0px;height: 0px;min-width: 80%;max-width: 80%;min-height: 80%;max-height: 80%;object-fit:cover;border-radius: 8px;vertical-align: top;' decoding='async'>
 											</span>
 											<div class='cont-info-offi-mrkt-b-ttrk'>
 												<p>" . ucwords($val['nombre_tienda']) . "</p>
@@ -53,21 +55,39 @@ $dat = $c->select_tienda();
 									</li>
 									";
 								}else{
-									echo "
-									<li class='item-str-off-into'>
-										<a href='productos?store={$val['nombre_tienda']}' class='item-cont-str-off btn_get_to_trademarks' >
-											<span class='cont-logo-offi-mrkt-b-ttrk' style='box-sizing: border-box;display: inline-block;overflow: hidden;width: initial;height: initial;background: none;opacity: 1;border: thin solid #ebebeb;margin: 0px;padding: 0px;position: relative;max-width: 100%;background-color: #fff;border-radius: 8px;'>
-												<span style='box-sizing: border-box; display: block; width: initial; height: initial; background: none; opacity: 1; border: 0px; margin: 0px; padding: 0px; max-width: 100%;'>
-													<img alt='' aria-hidden='true' src='data:image/svg+xml,%3csvg%20xmlns=%27http://www.w3.org/2000/svg%27%20version=%271.1%27%20width=%27138%27%20height=%27138%27/%3e' style='display: block; max-width: 100%; width: initial; height: initial; background: none; opacity: 1; border: 0px; margin: 0px; padding: 0px;'>
+									if(!file_exists($urlpath)){
+										echo "
+										<li class='item-str-off-into'>
+											<a href='productos?store={$val['nombre_tienda']}' class='item-cont-str-off btn_get_to_trademarks' >
+												<span class='cont-logo-offi-mrkt-b-ttrk' style='box-sizing: border-box;display: inline-block;overflow: hidden;width: initial;height: initial;background: none;opacity: 1;border: thin solid #ebebeb;margin: 0px;padding: 0px;position: relative;max-width: 100%;background-color: #fff;border-radius: 8px;'>
+													<span style='box-sizing: border-box; display: block; width: initial; height: initial; background: none; opacity: 1; border: 0px; margin: 0px; padding: 0px; max-width: 100%;'>
+														<img alt='' aria-hidden='true' src='data:image/svg+xml,%3csvg%20xmlns=%27http://www.w3.org/2000/svg%27%20version=%271.1%27%20width=%27138%27%20height=%27138%27/%3e' style='display: block; max-width: 100%; width: initial; height: initial; background: none; opacity: 1; border: 0px; margin: 0px; padding: 0px;'>
+													</span>
+													<img src='../Ttrueque/shop/images/store/default-store.png' alt='{$val['nombre_tienda']}' class='logo-off-mrkt-c-ttrk img-fluid' style='position: absolute;inset: 0px;box-sizing: border-box;padding: 0px;border: none;margin: auto;display: block;width: 0px;height: 0px;min-width: 80%;max-width: 80%;min-height: 80%;max-height: 80%;object-fit:cover;border-radius: 8px;vertical-align: top;' decoding='async'>
 												</span>
-												<img src='../Ttrueque/shop/images/store/{$val['logo']}' alt='{$val['nombre_tienda']}' class='logo-off-mrkt-c-ttrk img-fluid' style='position: absolute;inset: 0px;box-sizing: border-box;padding: 0px;border: none;margin: auto;display: block;width: 0px;height: 0px;min-width: 100%;max-width: 100%;min-height: 100%;max-height: 100%;object-fit:cover;border-radius: 8px;vertical-align: top;' decoding='async'>
-											</span>
-											<div class='cont-info-offi-mrkt-b-ttrk'>
-												<p>" . ucwords($val['nombre_tienda']) . "</p>
-											</div>
-										</a>
-									</li>
-									";
+												<div class='cont-info-offi-mrkt-b-ttrk'>
+													<p>" . ucwords($val['nombre_tienda']) . "</p>
+												</div>
+											</a>
+										</li>
+										";
+									}else{
+										echo "
+										<li class='item-str-off-into'>
+											<a href='productos?store={$val['nombre_tienda']}' class='item-cont-str-off btn_get_to_trademarks' >
+												<span class='cont-logo-offi-mrkt-b-ttrk' style='box-sizing: border-box;display: inline-block;overflow: hidden;width: initial;height: initial;background: none;opacity: 1;border: thin solid #ebebeb;margin: 0px;padding: 0px;position: relative;max-width: 100%;background-color: #fff;border-radius: 8px;'>
+													<span style='box-sizing: border-box; display: block; width: initial; height: initial; background: none; opacity: 1; border: 0px; margin: 0px; padding: 0px; max-width: 100%;'>
+														<img alt='' aria-hidden='true' src='data:image/svg+xml,%3csvg%20xmlns=%27http://www.w3.org/2000/svg%27%20version=%271.1%27%20width=%27138%27%20height=%27138%27/%3e' style='display: block; max-width: 100%; width: initial; height: initial; background: none; opacity: 1; border: 0px; margin: 0px; padding: 0px;'>
+													</span>
+													<img src='../Ttrueque/shop/images/store/{$val['logo']}' alt='{$val['nombre_tienda']}' class='logo-off-mrkt-c-ttrk img-fluid' style='position: absolute;inset: 0px;box-sizing: border-box;padding: 0px;border: none;margin: auto;display: block;width: 0px;height: 0px;min-width: 100%;max-width: 100%;min-height: 100%;max-height: 100%;object-fit:cover;border-radius: 8px;vertical-align: top;' decoding='async'>
+												</span>
+												<div class='cont-info-offi-mrkt-b-ttrk'>
+													<p>" . ucwords($val['nombre_tienda']) . "</p>
+												</div>
+											</a>
+										</li>
+										";
+									}
 								}
 							}
 						?>
--- views/official_markets.php
+++ views/official_markets.php
@@ -13,39 +13,59 @@
 				$slice_tiendas = array_slice($dat, 0, 10);
 
 				foreach ($slice_tiendas as $val){
-					if($val['logo'] == "default-store.png"){
-						echo "
-						<li class='item-str-off-into'>
-							<a href='productos?store={$val['nombre_tienda']}' class='item-cont-str-off btn_get_to_trademarks' >
-								<span class='cont-logo-offi-mrkt-b-ttrk' style='box-sizing: border-box;display: inline-block;overflow: hidden;width: initial;height: initial;background: none;opacity: 1;border: thin solid #ebebeb;margin: 0px;padding: 0px;position: relative;max-width: 100%;background-color: #fff;border-radius: 8px;'>
-									<span style='box-sizing: border-box; display: block; width: initial; height: initial; background: none; opacity: 1; border: 0px; margin: 0px; padding: 0px; max-width: 100%;'>
-										<img alt='' aria-hidden='true' src='data:image/svg+xml,%3csvg%20xmlns=%27http://www.w3.org/2000/svg%27%20version=%271.1%27%20width=%27138%27%20height=%27138%27/%3e' style='display: block; max-width: 100%; width: initial; height: initial; background: none; opacity: 1; border: 0px; margin: 0px; padding: 0px;'>
-									</span>
-									<img src='../Ttrueque/shop/images/store/{$val['logo']}' alt='{$val['nombre_tienda']}' class='logo-off-mrkt-c-ttrk img-fluid' style='position: absolute;inset: 0px;box-sizing: border-box;padding: 0px;border: none;margin: auto;display: block;width: 0px;height: 0px;min-width: 80%;max-width: 80%;min-height: 80%;max-height: 80%;object-fit:cover;border-radius: 8px;vertical-align: top;' decoding='async'>
-								</span>
-								<div class='cont-info-offi-mrkt-b-ttrk'>
-									<p>" . ucwords($val['nombre_tienda']) . "</p>
-								</div>
-							</a>
-						</li>
-						";
-					}else{
-						echo "
-						<li class='item-str-off-into'>
-							<a href='productos?store={$val['nombre_tienda']}' class='item-cont-str-off btn_get_to_trademarks' >
-								<span class='cont-logo-offi-mrkt-b-ttrk' style='box-sizing: border-box;display: inline-block;overflow: hidden;width: initial;height: initial;background: none;opacity: 1;border: thin solid #ebebeb;margin: 0px;padding: 0px;position: relative;max-width: 100%;background-color: #fff;border-radius: 8px;'>
-									<span style='box-sizing: border-box; display: block; width: initial; height: initial; background: none; opacity: 1; border: 0px; margin: 0px; padding: 0px; max-width: 100%;'>
-										<img alt='' aria-hidden='true' src='data:image/svg+xml,%3csvg%20xmlns=%27http://www.w3.org/2000/svg%27%20version=%271.1%27%20width=%27138%27%20height=%27138%27/%3e' style='display: block; max-width: 100%; width: initial; height: initial; background: none; opacity: 1; border: 0px; margin: 0px; padding: 0px;'>
-									</span>
-									<img src='../Ttrueque/shop/images/store/{$val['logo']}' alt='{$val['nombre_tienda']}' class='logo-off-mrkt-c-ttrk img-fluid' style='position: absolute;inset: 0px;box-sizing: border-box;padding: 0px;border: none;margin: auto;display: block;width: 0px;height: 0px;min-width: 100%;max-width: 100%;min-height: 100%;max-height: 100%;object-fit:cover;border-radius: 8px;vertical-align: top;' decoding='async'>
-								</span>
-								<div class='cont-info-offi-mrkt-b-ttrk'>
-									<p>" . ucwords($val['nombre_tienda']) . "</p>
-								</div>
-							</a>
-						</li>
-						";
-					}
+					$urlpath = $_SERVER['DOCUMENT_ROOT']."/Ttrueque/shop/images/store/".$val['logo'];
+								$urlpath_default = "../Ttrueque/shop/images/store/default-store.png";
+								if($val['logo'] == "default-store.png"){
+									echo "
+									<li class='item-str-off-into'>
+										<a href='productos?store={$val['nombre_tienda']}' class='item-cont-str-off btn_get_to_trademarks' >
+											<span class='cont-logo-offi-mrkt-b-ttrk' style='box-sizing: border-box;display: inline-block;overflow: hidden;width: initial;height: initial;background: none;opacity: 1;border: thin solid #ebebeb;margin: 0px;padding: 0px;position: relative;max-width: 100%;background-color: #fff;border-radius: 8px;'>
+												<span style='box-sizing: border-box; display: block; width: initial; height: initial; background: none; opacity: 1; border: 0px; margin: 0px; padding: 0px; max-width: 100%;'>
+													<img alt='' aria-hidden='true' src='data:image/svg+xml,%3csvg%20xmlns=%27http://www.w3.org/2000/svg%27%20version=%271.1%27%20width=%27138%27%20height=%27138%27/%3e' style='display: block; max-width: 100%; width: initial; height: initial; background: none; opacity: 1; border: 0px; margin: 0px; padding: 0px;'>
+												</span>
+												<img src='../Ttrueque/shop/images/store/default-store.png' alt='{$val['nombre_tienda']}' class='logo-off-mrkt-c-ttrk img-fluid' style='position: absolute;inset: 0px;box-sizing: border-box;padding: 0px;border: none;margin: auto;display: block;width: 0px;height: 0px;min-width: 80%;max-width: 80%;min-height: 80%;max-height: 80%;object-fit:cover;border-radius: 8px;vertical-align: top;' decoding='async'>
+											</span>
+											<div class='cont-info-offi-mrkt-b-ttrk'>
+												<p>" . ucwords($val['nombre_tienda']) . "</p>
+											</div>
+										</a>
+									</li>
+									";
+								}else{
+									if(!file_exists($urlpath)){
+										echo "
+										<li class='item-str-off-into'>
+											<a href='productos?store={$val['nombre_tienda']}' class='item-cont-str-off btn_get_to_trademarks' >
+												<span class='cont-logo-offi-mrkt-b-ttrk' style='box-sizing: border-box;display: inline-block;overflow: hidden;width: initial;height: initial;background: none;opacity: 1;border: thin solid #ebebeb;margin: 0px;padding: 0px;position: relative;max-width: 100%;background-color: #fff;border-radius: 8px;'>
+													<span style='box-sizing: border-box; display: block; width: initial; height: initial; background: none; opacity: 1; border: 0px; margin: 0px; padding: 0px; max-width: 100%;'>
+														<img alt='' aria-hidden='true' src='data:image/svg+xml,%3csvg%20xmlns=%27http://www.w3.org/2000/svg%27%20version=%271.1%27%20width=%27138%27%20height=%27138%27/%3e' style='display: block; max-width: 100%; width: initial; height: initial; background: none; opacity: 1; border: 0px; margin: 0px; padding: 0px;'>
+													</span>
+													<img src='../Ttrueque/shop/images/store/default-store.png' alt='{$val['nombre_tienda']}' class='logo-off-mrkt-c-ttrk img-fluid' style='position: absolute;inset: 0px;box-sizing: border-box;padding: 0px;border: none;margin: auto;display: block;width: 0px;height: 0px;min-width: 80%;max-width: 80%;min-height: 80%;max-height: 80%;object-fit:cover;border-radius: 8px;vertical-align: top;' decoding='async'>
+												</span>
+												<div class='cont-info-offi-mrkt-b-ttrk'>
+													<p>" . ucwords($val['nombre_tienda']) . "</p>
+												</div>
+											</a>
+										</li>
+										";
+									}else{
+										echo "
+										<li class='item-str-off-into'>
+											<a href='productos?store={$val['nombre_tienda']}' class='item-cont-str-off btn_get_to_trademarks' >
+												<span class='cont-logo-offi-mrkt-b-ttrk' style='box-sizing: border-box;display: inline-block;overflow: hidden;width: initial;height: initial;background: none;opacity: 1;border: thin solid #ebebeb;margin: 0px;padding: 0px;position: relative;max-width: 100%;background-color: #fff;border-radius: 8px;'>
+													<span style='box-sizing: border-box; display: block; width: initial; height: initial; background: none; opacity: 1; border: 0px; margin: 0px; padding: 0px; max-width: 100%;'>
+														<img alt='' aria-hidden='true' src='data:image/svg+xml,%3csvg%20xmlns=%27http://www.w3.org/2000/svg%27%20version=%271.1%27%20width=%27138%27%20height=%27138%27/%3e' style='display: block; max-width: 100%; width: initial; height: initial; background: none; opacity: 1; border: 0px; margin: 0px; padding: 0px;'>
+													</span>
+													<img src='../Ttrueque/shop/images/store/{$val['logo']}' alt='{$val['nombre_tienda']}' class='logo-off-mrkt-c-ttrk img-fluid' style='position: absolute;inset: 0px;box-sizing: border-box;padding: 0px;border: none;margin: auto;display: block;width: 0px;height: 0px;min-width: 100%;max-width: 100%;min-height: 100%;max-height: 100%;object-fit:cover;border-radius: 8px;vertical-align: top;' decoding='async'>
+												</span>
+												<div class='cont-info-offi-mrkt-b-ttrk'>
+													<p>" . ucwords($val['nombre_tienda']) . "</p>
+												</div>
+											</a>
+										</li>
+										";
+									}
+								}
 				}
 				?>
 				<!-- //END TO ITEMS-->
