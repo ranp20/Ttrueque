bea6f0c9e6ba847735ac9338aa4452d3d3d343e5
ranp20 <polancoranp2019@gmail.com>
2022-08-21 13:15:12 -0500

    repair sidebar-left and header-top into dashboard admin 1

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -160,14 +160,6 @@
       
     </main>
   </div>
-  <script type="text/javascript" src="js/jquery.min.js"></script>
-  <script type="text/javascript" src="js/bootstrap-select.min.js"></script>
-  <script type="text/javascript" src="js/bootstrap.min.js"></script>
-  <script type="text/javascript" src="js/jquery.dataTables.min.js"></script>
-  <script type="text/javascript" src="js/dataTables.bootstrap.min.js"></script>
-  <script type="text/javascript" src="js/Chart.min.js"></script>
-  <script type="text/javascript" src="js/fileinput.js"></script>
-  <script type="text/javascript" src="js/chartData.js"></script>
   <script type="text/javascript" src="js/main.js"></script>
 </body>
 </html>
--- admin/includes/adm_sidebar-left.php
+++ admin/includes/adm_sidebar-left.php
@@ -10,7 +10,7 @@ $admname = $_SESSION['user_admin'];
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" width="35px" height="35px"><path d="M310.6 361.4c12.5 12.5 12.5 32.75 0 45.25C304.4 412.9 296.2 416 288 416s-16.38-3.125-22.62-9.375L160 301.3L54.63 406.6C48.38 412.9 40.19 416 32 416S15.63 412.9 9.375 406.6c-12.5-12.5-12.5-32.75 0-45.25l105.4-105.4L9.375 150.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 210.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-105.4 105.4L310.6 361.4z"/></svg>
       </span>
       <div class="cSideBarLeft--sidenav--c--cLogo">
-        <img src="<?= $url;?>assets/img/logos/logo-negro.svg" alt="icon_preciodeldolar_adm" class="cSideBarLeft--sidenav--c--cLogo--img" width="100" height="100">
+        <img src="<?= $url;?>assets/img/logos/logotipo-T-white.svg" alt="icon_preciodeldolar_adm" class="cSideBarLeft--sidenav--c--cLogo--img" width="100" height="100">
       </div>
       <div class="cSideBarLeft--sidenav--c--cUserInfo">
         <div class="cSideBarLeft--sidenav--c--cUserInfo--cAvatar">
