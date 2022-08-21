0786a9d5884f5238117ab7f333f0b66dabd98e99
ranp20 <polancoranp2019@gmail.com>
2022-08-21 16:09:41 -0500

    repair styles into dashboard

--- admin/includes/adm_sidebar-left.php
+++ admin/includes/adm_sidebar-left.php
@@ -81,7 +81,7 @@ $admname = $_SESSION['user_admin'];
         <ul class="cSideBarLeft--sidenav--c--cList--mOthers">
           <li class="cSideBarLeft--sidenav--c--cList--mOthers--item">
             <a href="javascript:void(0);" class="cSideBarLeft--sidenav--c--cList--mOthers--link">
-              <svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="desktop" width="35px" height="35px" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class="svg-inline--fa fa-desktop fa-w-18 fa-3x"><path fill="currentColor" d="M528 0H48C21.5 0 0 21.5 0 48v288c0 26.5 21.5 48 48 48h480c26.5 0 48-21.5 48-48V48c0-26.5-21.5-48-48-48zm-6 336H54c-3.3 0-6-2.7-6-6V54c0-3.3 2.7-6 6-6h468c3.3 0 6 2.7 6 6v276c0 3.3-2.7 6-6 6zm-42 152c0 13.3-10.7 24-24 24H120c-13.3 0-24-10.7-24-24s10.7-24 24-24h98.7l18.6-55.8c1.6-4.9 6.2-8.2 11.4-8.2h78.7c5.2 0 9.8 3.3 11.4 8.2l18.6 55.8H456c13.3 0 24 10.7 24 24z" class=""></path></svg>
+              <svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="desktop" width="25px" height="25px" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class=""><path fill="currentColor" d="M528 0H48C21.5 0 0 21.5 0 48v288c0 26.5 21.5 48 48 48h480c26.5 0 48-21.5 48-48V48c0-26.5-21.5-48-48-48zm-6 336H54c-3.3 0-6-2.7-6-6V54c0-3.3 2.7-6 6-6h468c3.3 0 6 2.7 6 6v276c0 3.3-2.7 6-6 6zm-42 152c0 13.3-10.7 24-24 24H120c-13.3 0-24-10.7-24-24s10.7-24 24-24h98.7l18.6-55.8c1.6-4.9 6.2-8.2 11.4-8.2h78.7c5.2 0 9.8 3.3 11.4 8.2l18.6 55.8H456c13.3 0 24 10.7 24 24z" class=""></path></svg>
               <span>Ajustes del Home</span>
             </a>
           </li>
