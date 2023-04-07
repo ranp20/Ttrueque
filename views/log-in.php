4b365080b1abcce1d8c96b1748e757d15e79e0a3
ranp20 <polancoranp2019@gmail.com>
2023-04-06 22:54:39 -0500

    repair styles into project 2

--- views/log-in.php
+++ views/log-in.php
@@ -53,13 +53,13 @@ $paises = $all->get_name_country();
         <input type="hidden" name="estado" id="estado" value="<?php echo empty($estado) ? "" : $estado;?>">
         <div class="c-sectMain__cLogg__cFrm__cCtrls">
           <div class="c-sectMain__cLogg__cFrm__cCtrls__cIcon">
-            <img src="img/iconos_home/home-login-mail.svg" alt="email_svg" width="100" height="100">
+            <img src="img/iconos_home/home-login-mail.svg" class="img-fluid" alt="email_svg" width="100" height="100">
           </div>
           <input type="email" class="c-sectMain__cLogg__cFrm__cCtrls__ipt" required maxlength="200" name="email" id="email" placeholder="Correo electrónico o número de celular">
         </div>
         <div class="c-sectMain__cLogg__cFrm__cCtrls">
           <div class="c-sectMain__cLogg__cFrm__cCtrls__cIcon">
-            <img src="img/iconos_home/home-login-lock.svg" alt="password_svg" width="100" height="100">
+            <img src="img/iconos_home/home-login-lock.svg" class="img-fluid" alt="password_svg" width="100" height="100">
           </div>
           <input type="password" autocomplete="true" class="c-sectMain__cLogg__cFrm__cCtrls__ipt" required maxlength="40" name="pass" id="password_in" placeholder="Ingrese su contraseña">
         </div>
