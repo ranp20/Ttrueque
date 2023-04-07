7315cdc0a6933b599482652316652cedf730b780
ranp20 <polancoranp2019@gmail.com>
2023-04-07 12:11:40 -0500

    repair styles into project 4

--- views/recover-password.php
+++ views/recover-password.php
@@ -13,9 +13,14 @@ if (isset($_SESSION["user"])){
   <?php require_once 'includes/header_links.php'; ?>
 </head>
 <body>
-	<?php require_once '../view_home/presentacion_header_texto_b.php';?>
+	<?php // require_once '../view_home/presentacion_header_texto_b.php';?>
 	<div class="cont-recover-password">
 		<div class="cont-form-rec">
+			<div class="c-sectMain__cAcco__cAcct--cLogo">
+				<a href="./" class="c-sectMain__cAcco__cAcct--cLogo--link">
+          <img src="img/logo/Login_Web-T-blue.svg" alt="logo_Ttrueque" class="img-fluid" width="100" height="100">
+        </a>
+			</div>
 			<form class="cont-form-rec--form" method="POST" id="recover-pass">
 			  <h3 class="cont-form-rec--form--title">RECUPERAR CONTRASEÑA</h3>
 				<p class="cont-form-rec--form--description">Introduce tu correo electrónico de usuario para recibir instrucciones de como reestablecer tu contraseña.</p>
@@ -27,7 +32,13 @@ if (isset($_SESSION["user"])){
           </div>
 					<input type="text" name="mail-recover" class="cont-form-rec--form--control--input" required maxlength="200" placeholder="Correo electrónico o número de celular">
 				</div>
-				<button type="submit" class="cont-form-rec--form--btnsendemail">ENVIAR</button>
+				<button type="submit" class="cont-form-rec--form--btnsendemail">SOLICITAR</button>
+				<div class="cont-form-rec--form--cBckLgg">
+					<a href="login" class="cont-form-rec--form--cBckLgg--link">
+						<svg xmlns="http://www.w3.org/2000/svg" width="45px" height="45px" version="1.1" viewBox="0 0 700 700"><path d="m455.41 268.47h-179.53l77.41-77.41c4.9414-4.9414 4.9414-13.176 0-18.117-4.9414-4.9414-11.531-4.9414-16.469 0l-98.824 98.824c-4.9414 4.9414-4.9414 13.176 0 18.117l98.824 98.824c4.9414 4.9414 13.176 4.9414 18.117 0 4.9414-4.9414 4.9414-11.531 0-16.469l-77.41-79.059h179.53c6.5898 0 13.176-4.9414 13.176-13.176-3.2969-6.5938-8.2383-13.18-14.828-11.535z"/></svg>
+						<span>Regresar para iniciar sesión</span>
+					</a>
+				</div>
 			</form>
 		</div>
 	</div>
